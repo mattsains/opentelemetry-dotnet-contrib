0409ea1c80e95fdea3cd1d9ca2681cb62fac2c85
Matthew Sainsbury <matthew@sainsbury.io>
2026-02-13 15:36:11 -0800

    quick test

--- examples/process-instrumentation/Program.cs
+++ examples/process-instrumentation/Program.cs
@@ -1,19 +1,41 @@
 // Copyright The OpenTelemetry Authors
 // SPDX-License-Identifier: Apache-2.0
 
+using System.Diagnostics;
 using OpenTelemetry;
+using OpenTelemetry.Exporter.Geneva;
 using OpenTelemetry.Metrics;
+using OpenTelemetry.Trace;
 
 internal class Program
 {
     public static void Main()
     {
-        using var meterProvider = Sdk.CreateMeterProviderBuilder()
-            .AddProcessInstrumentation()
-            .AddPrometheusHttpListener()
+        var path = GetRandomFilePath();
+        using var tracerProvider = Sdk.CreateTracerProviderBuilder()
+            .SetSampler(new AlwaysOnSampler())
+            .AddSource("*")
+            .AddGenevaTraceExporter(options =>
+            {
+                options.ConnectionString = "Endpoint=unix:" + path;
+            })
             .Build();
 
-        Console.WriteLine(".NET Process metrics are available at http://localhost:9464/metrics, press any key to exit...");
-        Console.ReadKey(false);
+        using var activity = new Activity("hello");
+        tracerProvider.ForceFlush();
+        Thread.Sleep(10000);
+    }
+
+    private static string GetRandomFilePath()
+    {
+        while (true)
+        {
+            var path = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+            if (!File.Exists(path))
+            {
+                Console.WriteLine(path);
+                return path;
+            }
+        }
     }
 }
